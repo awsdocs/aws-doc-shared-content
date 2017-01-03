f8a0b2ea538251046c508641cc874f3bc12f2dd5
Hennessey <eronh@amazon.com>
2017-01-03 12:01:24 -0800

    add shared region include and update copyright date for 2017

--- sphinx_shared/_conf/default_extlinks.py
+++ sphinx_shared/_conf/default_extlinks.py
@@ -1,4 +1,4 @@
-# Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Copyright 2010-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
 # External link (intersphinx + extlink) definitions
 
